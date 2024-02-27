72e23253a199c49a25dd80eedc1e99de327aa4cb
burhanHere <burhanburewala@gmail.com>
2024-02-27 22:30:45 +0500

    sreated search bar can filters

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.css'],
 })
 export class FilterComponent {
-
+  @Input()
+  allProductsCount!: number;
+  @Input()
+  inStockProductsCount!: number;
+  @Input()
+  outOfStockProductsCount!: number;
+  @Input()
+  selectedFilter = 'all';
+  @Output()
+  OnFilterSelect: EventEmitter<string> = new EventEmitter<string>();
 }
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent {
+  searchText: string = 'NULL_NODATA';
 
+  @Output()
+  OnSearchButtonClick: EventEmitter<string> = new EventEmitter<string>();
+
+  emitSearchText(): void {
+    this.OnSearchButtonClick.emit(this.searchText);
+  }
 }
