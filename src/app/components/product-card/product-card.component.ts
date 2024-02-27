f94ad9e6955ee46ee99c891c4f610bf46c95ca28
burhanHere <burhanburewala@gmail.com>
2024-02-28 02:44:20 +0500

    completed the product card component

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.css']
+  styleUrls: ['./product-card.component.css'],
 })
 export class ProductCardComponent {
-
+  @Input()
+  product: Product = {} as Product;
+  @Output()
+  onProductCardClick: EventEmitter<Product> = new EventEmitter<Product>();
 }
