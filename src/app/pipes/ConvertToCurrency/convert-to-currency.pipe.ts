2c89557a487cb9e422a943e5096ff86b1e6b0607
burhanHere <burhanburewala@gmail.com>
2024-03-12 21:11:39 +0500

    fixed the errors in api in both pipes and service created gfor currency rate exchnage

--- src/app/pipes/ConvertToCurrency/convert-to-currency.pipe.ts
+++ src/app/pipes/ConvertToCurrency/convert-to-currency.pipe.ts
@@ -5,21 +5,27 @@ import { CurrencyRatesService } from 'src/app/services/CurrencyRates/currency-ra
   name: 'convertToCurrency',
 })
 export class ConvertToCurrencyPipe implements PipeTransform {
-  currencyRatesServiceResponce: any;
+  serviceResponse: any;
   constructor(private currencyRatesService: CurrencyRatesService) {}
 
   transform(value: number, ...args: any[]): number {
     const [baseCurrency, targetCurrency] = args;
-    console.log('in pipe: ' + value);
-    console.log('in pipe:: baseCurrency ' + baseCurrency);
-    console.log('in pipe:: targetCurrency ' + targetCurrency);
-    // code to make api call and convert to the respected currency then return the value
-    this.currencyRatesService
-      .getCurrencyRates(baseCurrency, targetCurrency)
-      .subscribe((response) => {
-        this.currencyRatesServiceResponce = response;
-      });
-    console.log(this.currencyRatesServiceResponce);
+    if (baseCurrency != targetCurrency) {
+      // code to make api call and convert to the respected currency then return the value
+      this.currencyRatesService
+        .getCurrencyRates(baseCurrency, targetCurrency)
+        .then((responce) => {
+          this.serviceResponse = responce;
+          console.log(this.serviceResponse);
+          console.log(this.serviceResponse.body.data[targetCurrency]);
+          value *= this.serviceResponse.body.data[targetCurrency];
+          console.log(value);
+          return value;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
     return value;
   }
 }
--- src/app/services/CurrencyRates/currency-rates.service.ts
+++ src/app/services/CurrencyRates/currency-rates.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable, observeOn } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyRatesService {
-  constructor(private client: HttpClient) {}
+  private ApiUrl: string;
+  constructor(private client: HttpClient) {
+    this.ApiUrl = '';
+  }
 
-  private baseApiUrl: string =
-    'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_9q7JGvcG5zgMyWEWdwoiQIvEc7M7Su26HebWSbBP&';
+  // getCurrencyRates(
+  //   baseCurrency: string,
+  //   targetCurrency: string
+  // ): Observable<HttpResponse<any>> {
+  //   this.ApiUrl = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_9q7JGvcG5zgMyWEWdwoiQIvEc7M7Su26HebWSbBP&currencies=${targetCurrency}&base_currency=${baseCurrency}`;
+  //   console.log('in service:: this.ApiUrl:' + this.ApiUrl);
+  //   const response: any = this.client.get(this.ApiUrl, { observe: 'response' });
+  //   return response;
+  // }
 
-  getCurrencyRates(
-    baseCurrency: string,
-    targetCurrency: string
-  ): Observable<HttpResponse<any>> {
-    let requestedCurrecy: string = `currencies=${targetCurrency}&base_currency=${baseCurrency}`;
-    let requestApiURL: string = this.baseApiUrl + requestedCurrecy;
-    console.log(requestApiURL);
-    return this.client.get(requestApiURL, { observe: 'response' });
+  async getCurrencyRates(baseCurrency: string, targetCurrency: string) {
+    //  Promise<HttpResponse<Object>>;
+    const url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_9q7JGvcG5zgMyWEWdwoiQIvEc7M7Su26HebWSbBP&currencies=${targetCurrency}&base_currency=${baseCurrency}`;
+
+    try {
+      const response = await this.client
+        .get(url, { observe: 'response' })
+        .toPromise();
+      return response;
+    } catch (error) {
+      return Object({ data: error });
+    }
   }
 }
