c19aea4ac2e694fe48c83f671729d8f6cce0a7f5
burhanHere <burhanburewala@gmail.com>
2024-03-16 16:59:00 +0500

    Edited conatct page, updaed form and validations in the foms

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  name?: string;
+  email?: string;
+  subject?: string;
+  message?: string;
 
+  constructor() {
+    this.name = '';
+    this.email = '';
+    this.subject = '';
+    this.message = '';
+  }
+
+  postData(contactForm: any): void {
+    console.log(contactForm);
+    console.log('-------------------------');
+    console.log(this.name);
+    console.log(this.email);
+    console.log(this.subject);
+    console.log(this.message);
+    //code here t send data to the backend or api
+  }
 }
